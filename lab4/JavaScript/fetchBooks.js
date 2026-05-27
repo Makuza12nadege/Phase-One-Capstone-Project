9cdaf61489da8c1143dbc1cd6ff93732e65f3bbf
Makuza12nadege <nadegemakuza@gmail.com>
2026-05-27 15:29:24 +0200

    Phase One Capstone Project - All Labs

---
+++ lab4/JavaScript/fetchBooks.js
@@ -0,0 +1,11 @@
+export async function fetchBooks(query) {
+  const response = await fetch(`https://openlibrary.org/search.json?q=${query}&limit=6`);
+  const data = await response.json();
+
+  return data.docs.map(book => ({
+    id: book.key,
+    title: book.title,
+    author: book.author_name ? book.author_name[0] : "Unknown Author",
+    cover: book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : null,
+  }));
+}
